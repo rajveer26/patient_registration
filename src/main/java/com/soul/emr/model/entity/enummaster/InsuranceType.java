a9c013f717cf66c04589ed7d3506e570c4f07b7b
Divyesh Nag <nag.divyesh@yahoo.com>
2024-12-17 14:57:09 +0530

    updated

--- src/main/java/com/soul/emr/model/entity/enummaster/InsuranceType.java
+++ src/main/java/com/soul/emr/model/entity/enummaster/InsuranceType.java
@@ -1,27 +1,6 @@
 package com.soul.emr.model.entity.enummaster;
 
-//public enum InsuranceType
-//{
-//    Self, TPA, Corporate, PED, Insurance
-//}
-
-
-import lombok.Getter;
-
-@Getter
-public enum InsuranceType {
-    SELF(1, "Self"),
-    TPA(2, "TPA"),
-    CORPORATE(3, "Corporate"),
-    PED(4, "PED"),
-    INSURANCE(5, "Insurance");
-
-    private final int id;
-    private final String value;
-
-    InsuranceType(int id, String value) {
-        this.id = id;
-        this.value = value;
-    }
-
+public enum InsuranceType
+{
+    Self, TPA, Corporate, PED, Insurance
 }

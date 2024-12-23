30c1651b20b7e3990bd2b17fe21cd2c0708bba7e
Rajveer <rajveer26ps@gmail.com>
2024-12-23 21:19:07 +0530

    changes in patient registration

--- src/main/java/com/soul/patient/model/repository/jparepository/patientrepository/PatientConsultationRepository.java
+++ src/main/java/com/soul/patient/model/repository/jparepository/patientrepository/PatientConsultationRepository.java
@@ -19,18 +19,18 @@ public interface PatientConsultationRepository extends JpaRepository<PatientCons
 	
 	// Query to count the records based on optional encounterDates and type
 	@Transactional
-	@Query("SELECT COUNT(p) FROM PatientConsultationDB p WHERE (:type IS NULL OR p.type = :type) AND p.doctorCode.doctorCode = :doctorCode")
+	@Query("SELECT COUNT(p) FROM PatientConsultationDB p WHERE (:type IS NULL OR p.type = :type) AND p.doctorCode = :doctorCode")
 	long findByPatientConsultationCount(@Param("type") String type, @Param("doctorCode") String doctorCode);
 	
 	//Query to fetch patient count for graph with gender and encounterDate
 	@Transactional
-	@Query("SELECT p.patientDetailDB.gender, p.encounterDate, COUNT(p) FROM PatientConsultationDB p WHERE (:type IS NULL OR p.type = :type) AND p.doctorCode.doctorCode = :doctorCode AND p.encounterDate >= :currentDate AND p.encounterDate < :currentDate GROUP BY p.patientDetailDB.gender, p.encounterDate")
+	@Query("SELECT p.patientDetailDB.gender, p.encounterDate, COUNT(p) FROM PatientConsultationDB p WHERE (:type IS NULL OR p.type = :type) AND p.doctorCode = :doctorCode AND p.encounterDate >= :currentDate AND p.encounterDate < :currentDate GROUP BY p.patientDetailDB.gender, p.encounterDate")
 	List <Object[]> findPatientCountGenderAndEncounterDate(@Param("type") String type, @Param("doctorCode") String doctorCode, @Param("currentDate") LocalDate currentDate);
 	
 	
 	//Query to fetch patient count based on consultation type
 	@Transactional
-	@Query("SELECT COUNT(p) FROM PatientConsultationDB p WHERE p.consultationType IN :consultationTypes AND p.doctorCode.doctorCode = :doctorCode AND p.encounterDate = :currentDate")
+	@Query("SELECT COUNT(p) FROM PatientConsultationDB p WHERE p.consultationType IN :consultationTypes AND p.doctorCode = :doctorCode AND p.encounterDate = :currentDate")
 	long findPatientCountBasedOnConsultationType(@Param("consultationTypes") List<String> consultationTypes, @Param("doctorCode") String doctorCode, @Param("currentDate") LocalDate currentDate);
 	
 }
